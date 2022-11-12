21be86d50a2e2fae9755f78850f90c4206c73b75
truong <willnguyen2971990@gmail.com>
2022-11-12 21:48:45 +0700

    init

---
+++ auth/README.md
@@ -0,0 +1,10 @@
+ts-node-dev: hot load node when files change.
+express: web framework for node.js
+@types/express: type definition for express
+typescript: a language for application-scale javascript. add optional types to js.
+express-async-errors: return errors async
+
+ingres: https://kubernetes.github.io/ingress-nginx/deploy/#quick-start
+```
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.5.1/deploy/static/provider/cloud/deploy.yaml
+```
---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,38 @@
+import express, { Request, Response } from 'express'
+import { body, validationResult } from 'express-validator' // https://express-validator.github.io/docs/
+import { RequestValidationError } from '../errors/request-validation-error'
+import { DatabaseConnectionError } from '../errors/database-connection-error'
+import { User } from '../models/user'
+
+const router = express.Router()
+
+router.post('/api/users/signup', [
+    body('email')
+        .isEmail()
+        .withMessage('Email is invalid'),
+    body('password')
+        .trim()
+        .isLength({min: 4, max: 20})
+        .withMessage('Password must be between 4 and 40 chars')
+],
+async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()){
+        throw new RequestValidationError(errors.array())
+    }
+    console.log('Creating user...')
+
+    const { email, password } = req.body 
+    const existingUser = await User.findOne({ email })
+    if (existingUser) {
+        console.log('Email in use')
+        return res.send({})
+    }
+    console.log(`existingUser: ${existingUser}`)
+    const user = User.build({ email, password })
+    await user.save()
+    console.log('user created ')
+    res.status(201).send(user)
+})
+export { router as signupRouter };
